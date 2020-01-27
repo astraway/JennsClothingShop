555445e002cc3874230bdd0bd68061d0c7b1de2c
Eric Jurotich <ejurotic@cisco.com>
2020-01-26 19:07:47 -0800

    Added EF nuget, wired it up. Need to implement the real db repositories now.

--- Startup.cs
+++ Startup.cs
@@ -8,15 +8,26 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using JennsClothingShop.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace JennsClothingShop
 {
     public class Startup
     {
+        //info about configuration https://app.pluralsight.com/course-player?clipId=0c410026-c23f-44bb-b778-c1c871f898eb
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddScoped<IDressRepositry, MockDressRepository>();
             services.AddScoped<ICategoryRepository, MockCategoryRepository>();
             services.AddControllersWithViews();
